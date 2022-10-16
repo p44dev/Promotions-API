b6da75681269540739025728a076c86b177c924c
Anton <p44dev@gmail.com>
2022-10-16 18:56:57 +0400

    - Removed redudant comments

--- controllers/promotion_controller.go
+++ controllers/promotion_controller.go
@@ -7,33 +7,8 @@ import (
 	"PromotionsAPI/models"
 
 	"github.com/gin-gonic/gin"
-
-	// "github.com/google/uuid"
 	"net/http"
-	// "time"
 )
-
-// const DATE_TIME_FORMAT string = "2006-01-02 15:04:05"
-
-// func CreatePromotions(c *gin.Context) {
-
-// 	uuid := uuid.New()
-
-// 	promotion := models.Promotion{
-// 		UUID:           uuid,
-// 		Price:          1.23,
-// 		ExpirationDate: time.Now().Format(DATE_TIME_FORMAT),
-// 	}
-
-// 	result := initializers.DB.Create(&promotion)
-
-// 	if result.Error != nil {
-// 		c.Status(http.StatusNotFound)
-// 		return
-// 	}
-
-// 	c.JSON(http.StatusOK, promotion)
-// }
 
 func GetPromotions(context *gin.Context) {
 
